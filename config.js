6dcecacea360f3bcc1399bb9eaa966686acf9e35
halfgreen801 <quiec@naver.com>
2026-06-24 22:21:33 +0900

    Disable public signup

--- config.js
+++ config.js
@@ -2,5 +2,5 @@ window.TUBE_VAULT_CONFIG = {
   syncEnabled: true,
   supabaseUrl: "https://uhwqzujuerceoysiuupa.supabase.co",
   supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVod3F6dWp1ZXJjZW95c2l1dXBhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODIyOTQ0NjcsImV4cCI6MjA5Nzg3MDQ2N30.n6gAeN28zZK3vEl3IYTWvwbkq5a_CJYVlwAtWYFS30o",
-  allowSignup: true
+  allowSignup: false
 };
